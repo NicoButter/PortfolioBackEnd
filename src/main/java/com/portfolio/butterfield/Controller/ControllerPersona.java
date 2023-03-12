5bd83a31e84af6179efc649ca35531fb32b540fd
NicoButter <nicobutter@gmail.com>
2023-03-12 17:49:04 -0300

    origins

--- src/main/java/com/portfolio/butterfield/Controller/ControllerPersona.java
+++ src/main/java/com/portfolio/butterfield/Controller/ControllerPersona.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://portfolioftenicolasbutterfield.web.app"})
 public class ControllerPersona {
     
     @Autowired
