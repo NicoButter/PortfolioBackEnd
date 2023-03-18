6bcbccdefa0a602c58f751446a9711ce6fbedc2f
NicoButter <nicobutter@gmail.com>
2023-03-18 15:24:48 -0300

    correccion_4200_auth

--- src/main/java/com/portfolio/butterfield/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/butterfield/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://portfolioftenicolasbutterfield.web.app")
+@CrossOrigin(origins = {"http://localhost:4200", "https://portfolioftenicolasbutterfield.web.app"})
 public class AuthController {
    
     @Autowired
