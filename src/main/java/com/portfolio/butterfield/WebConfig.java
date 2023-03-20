154c4bf54ec35fdb189e35c86539fcc5f24adc40
NicoButter <nicobutter@gmail.com>
2023-03-20 17:04:19 -0300

    correccion_1940

--- src/main/java/com/portfolio/butterfield/WebConfig.java
+++ src/main/java/com/portfolio/butterfield/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("https://portfolioftenicolasbutterfield.web.app");
+        //registry.addMapping("https://portfoliofrontendbutterfield.web.app");
         registry.addMapping("/**");
     }
 }
