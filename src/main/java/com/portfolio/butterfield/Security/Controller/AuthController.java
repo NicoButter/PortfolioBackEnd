57fcf9423251eb261fe958d316b8c981eb92c43b
NicoButter <nicobutter@gmail.com>
2023-04-02 14:17:18 -0300

    cambios

--- src/main/java/com/portfolio/butterfield/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/butterfield/Security/Controller/AuthController.java
@@ -29,8 +29,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("auth/")
-@CrossOrigin(origins = {"http://localhost:4200/", "https://portfoliofrontendbutterfield.web.app/"})
+@RequestMapping("/auth")
+@CrossOrigin(origins = {"http://localhost:4200/", "https://portfoliofrontendbutterfield.web.app"})
 public class AuthController {
    
     @Autowired
@@ -48,7 +48,7 @@ public class AuthController {
     @Autowired
     JwtProvider jwtProvider;
     
-    @PostMapping("nuevo/")
+    @PostMapping("/nuevo")
     public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
        
         if(bindingResult.hasErrors())
@@ -73,7 +73,7 @@ public class AuthController {
         return new ResponseEntity(new Mensaje("Usuario guardado con éxito."), HttpStatus.CREATED);
     }
     
-    @PostMapping("login/")
+    @PostMapping("/login")
     public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
         
         if(bindingResult.hasErrors())
